d4aad0a1465816b8925fedf83f0ab9aa813bd5ab
Zamdange <bazange96@gmail.com>
2021-11-04 16:15:13 +0200

    changes

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','house_rental_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('sql11.freemysqlhosting.net','sql11448949','BUUVMPXDEM','sql11448949')or die("Could not connect to mysql".mysqli_error($con));
