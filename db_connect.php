0252b7f3fb608853e165bd4b9838193b44a50658
Zamdange <bazange96@gmail.com>
2021-11-04 16:46:35 +0200

    changes

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,8 @@
 <?php 
 
-$conn= new mysqli('sql11.freemysqlhosting.net','sql11448949','BUUVMPXDEM','sql11448949')or die("Could not connect to mysql".mysqli_error($con));
+if($_SERVER["REMOTE_ADDR"]=="127.0.0.1"){
+    $conn= new mysqli('localhost','root','','house_rental_db')or die("Could not connect to mysql".mysqli_error($con));
+    $local = True;
+    }else{
+        $conn= new mysqli('sql11.freemysqlhosting.net','sql11448949','BUUVMPXDEM','sql11448949')or die("Could not connect to mysql".mysqli_error($con));
+    }
